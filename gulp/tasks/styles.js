f137931cb6f1cf00552392dcc0271f5464fda596
Long Nguyen <longnt80@gmail.com>
2017-09-07 07:40:27 +0700

    Added sourcemaps

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,14 +4,17 @@ autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 cssnested = require('postcss-nested'),
 cssImport = require('postcss-import'),
-mixins = require('postcss-mixins');
+mixins = require('postcss-mixins'),
+sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('styles', function() {
   return gulp.src('./app/assets/styles/styles.css')
+    .pipe(sourcemaps.init())
     .pipe(postcss([cssImport, mixins, cssnested, cssvars, autoprefixer]))
     .on('error', function(errorInfo) {
       console.log(errorInfo.toString());
       this.emit('end');
     })
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('./app/temp/styles'));
 });
